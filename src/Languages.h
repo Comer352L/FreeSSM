b472f88a112e89d00e31558bfa84c4eba0be8c22
Comer352L <Comer352L@googlemail.com>
2011-01-02 21:57:52 +0100

    Add missing file src/Languages.h
    Should have been added with commit 6a4734a47e49a0c2f8f6b3c18b46c3c9a8b2a685 "Make adding/removing of program languages easier".

---
+++ src/Languages.h
@@ -0,0 +1,42 @@
+/*
+ * Languages.h - Supported program languages
+ *
+ * Copyright (C) 2010 Comer352l
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LANGUAGES_H
+#define LANGUAGES_H
+
+
+#include <QVector>
+#include <QStringList>
+
+
+/* Supported locales (languages) */
+static const QVector<QLocale> __supportedLocales = QVector<QLocale>()
+                                                   << QLocale::English
+                                                   << QLocale::German;
+                                                   /* ===> ADD NEW LOCALES HERE <=== */
+
+/* Make language names translatable */
+#if 0
+QT_TRANSLATE_NOOP("Language", "English")
+QT_TRANSLATE_NOOP("Language", "German")
+/* ===> ADD NEW LANGUAGE NAMES HERE <=== */
+#endif
+
+
+#endif
