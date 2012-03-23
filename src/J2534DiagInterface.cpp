Comer352L/FreeSSM

commit 25322090c72b88bbf4ff138e8e7b730eed0ad263

Fix some compilation warnings

src/J2534DiagInterface.cpp
@@ -309,11 +309,11 @@ bool J2534DiagInterface::connect(AbstractDiagInterface::protocol_type protocol)
 		return false;
 
 err_close:
-	long ret = 0;
-	ret = _j2534->PassThruDisconnect(_ChannelID);
 #ifdef __FSSM_DEBUG__
-	if (STATUS_NOERROR != ret)
+	if (STATUS_NOERROR != _j2534->PassThruDisconnect(_ChannelID))
 		printErrorDescription("PassThruDisconnect() failed: ", ret);
+#else
+	_j2534->PassThruDisconnect(_ChannelID);
 #endif
 	return false;
 }

src/SSMprotocol1.cpp
@@ -300,7 +300,7 @@ bool SSMprotocol1::isEngineRunning(bool *isrunning)
 {
 	if (_state != state_normal) return false;
 
-	// TODO !
+(void)*isrunning;	// TODO !
 	
 return false;
 }
@@ -326,7 +326,7 @@ bool SSMprotocol1::clearMemory(CMlevel_dt level, bool *success)
 bool SSMprotocol1::testImmobilizerCommLine(immoTestResult_dt *result)
 {
 
-	// TODO !
+(void)*result;	// TODO !
 
 return false;
 }
@@ -402,12 +402,12 @@ bool SSMprotocol1::stopDCreading()
 
 void SSMprotocol1::processDCsRawdata(std::vector<char> DCrawdata, int duration_ms)
 {
+	(void)duration_ms; // to avoid compiler error
 	QStringList DCs;
 	QStringList DCdescriptions;
 	QStringList tmpDTCs;
 	QStringList tmpDTCsDescriptions;
 	unsigned int DCsRawValueIndex = 0;
-	duration_ms = 0; // to avoid compiler error
 	if (_selectedDCgroups & currentDTCs_DCgroup)
 	{
 		DCs.clear();

src/SSMprotocol2.cpp
@@ -1257,6 +1257,7 @@ bool SSMprotocol2::stopDCreading()
 
 void SSMprotocol2::processDCsRawdata(std::vector<char> DCrawdata, int duration_ms)
 {
+	(void)duration_ms; // to avoid compiler error
 	QStringList DCs;
 	QStringList DCdescriptions;
 	QStringList tmpDTCs;
@@ -1265,7 +1266,6 @@ void SSMprotocol2::processDCsRawdata(std::vector<char> DCrawdata, int duration_m
 	bool DCheckActive = false;
 	unsigned int DCsAddrIndex = 0;
 	unsigned int DCsAddrIndexOffset = 0;
-	duration_ms = 0; // to avoid compiler error
 	if ((_selectedDCgroups & currentDTCs_DCgroup) || (_selectedDCgroups & temporaryDTCs_DCgroup))
 	{
 		if (_CU == CUtype_Engine)

commit 1337beadbdd58551ea80f4714389ad74511b3740

Fix debug compilation
Commit 25322090c72b88bbf4ff138e8e7b730eed0ad263 broke debug compilation.

src/J2534DiagInterface.cpp
@@ -310,7 +310,8 @@ bool J2534DiagInterface::connect(AbstractDiagInterface::protocol_type protocol)
 
 err_close:
 #ifdef __FSSM_DEBUG__
-	if (STATUS_NOERROR != _j2534->PassThruDisconnect(_ChannelID))
+	long ret = _j2534->PassThruDisconnect(_ChannelID);
+	if (STATUS_NOERROR != ret)
 		printErrorDescription("PassThruDisconnect() failed: ", ret);
 #else
 	_j2534->PassThruDisconnect(_ChannelID);