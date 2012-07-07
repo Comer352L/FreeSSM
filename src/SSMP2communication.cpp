ecf523fe74dfc1a7c30449eba5661e53682a950e
Comer352L <Comer352L@googlemail.com>
2012-07-07 12:46:48 +0200

    SSMP2communication: fix calculation of block read index
    This fixes an ancient bug.

--- src/SSMP2communication.cpp
+++ src/SSMP2communication.cpp
@@ -453,7 +453,7 @@ void SSMP2communication::run()
 			if (errcount > 0)
 				errcount--;
 			// Set query-index:
-			if ( ((operation == comOp_readMulti) || (operation == comOp_readMulti_p)) && (rindex < static_cast<unsigned int>((datalen/max_bytes_per_multiread)+1)) )
+			if ( ((operation == comOp_readMulti) || (operation == comOp_readMulti_p)) && (rindex < static_cast<unsigned int>(((datalen-1)/max_bytes_per_multiread)+1)) )
 				rindex++;
 			else
 				rindex=1;
