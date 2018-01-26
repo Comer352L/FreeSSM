fb0f1ecd8a928b82e4a950cca31841e3f61c3fc7
Comer352L <comer352l@googlemail.com>
2018-01-26 17:09:01 +0100

    CUinfo_simple: fonts: use point sizes instead of pixel sizes
    Font scaling on MS Windows works much better these days, at least
    when using resolutions >= 1024x768.
    So we can switch back to using the "natural" font size mode, which
    gives the operating system more control over font scaling.
    
    This allows us to get rid of the fonts reconfiguration, since the
    font sizes in points are already specified in the ui-file.

--- src/CUinfo_simple.cpp
+++ src/CUinfo_simple.cpp
@@ -1,7 +1,7 @@
 /*
  * CUinfo_simple.cpp - Basic widget for displaying Control Unit information
  *
- * Copyright (C) 2008-2012 Comer352L
+ * Copyright (C) 2008-2018 Comer352L
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -23,7 +23,6 @@
 CUinfo_simple::CUinfo_simple(QWidget * parent) : QWidget(parent)
 {
 	setupUi(this);
-	setupUiFonts();
 }
 
 
@@ -48,53 +47,5 @@ void CUinfo_simple::setNrOfSupportedMBsSWs(unsigned int MBs, unsigned int SWs)
 {
 	nrofdatambs_label->setText( QString::number(MBs, 10) );
 	nrofswitches_label->setText( QString::number(SWs, 10) );
-}
-
-
-void CUinfo_simple::setupUiFonts()
-{
-	// SET FONT FAMILY AND FONT SIZE
-	// OVERWRITES SETTINGS OF ui_CUinfo_Transmission.h (made with QDesigner)
-	QFont appfont = QApplication::font();
-	QFont font = this->font();
-	font.setFamily(appfont.family());
-	font.setPixelSize(12);	// 9pts
-	this->setFont(font);
-	font = systemTypeTitle_label->font();
-	font.setFamily(appfont.family());
-	font.setPixelSize(12);	// 9pts
-	systemTypeTitle_label->setFont(font);
-	font = systemType_label->font();
-	font.setFamily(appfont.family());
-	font.setPixelSize(12);	// 9pts
-	systemType_label->setFont(font);
-	font = romIDtitle_label->font();
-	font.setFamily(appfont.family());
-	font.setPixelSize(12);	// 9pts
-	romIDtitle_label->setFont(font);
-	font = romID_label->font();
-	font.setFamily(appfont.family());
-	font.setPixelSize(12);	// 9pts
-	romID_label->setFont(font);
-	font = nrofmbsswstitle_label->font();
-	font.setFamily(appfont.family());
-	font.setPixelSize(12);	// 9pts
-	nrofmbsswstitle_label->setFont(font);
-	font = nrofdatambstitle_label->font();
-	font.setFamily(appfont.family());
-	font.setPixelSize(12);	// 9pts
-	nrofdatambstitle_label->setFont(font);
-	font = nrofdatambs_label->font();
-	font.setFamily(appfont.family());
-	font.setPixelSize(12);	// 9pts
-	nrofdatambs_label->setFont(font);
-	font = nrofswitchestitle_label->font();
-	font.setFamily(appfont.family());
-	font.setPixelSize(12);	// 9pts
-	nrofswitchestitle_label->setFont(font);
-	font = nrofswitches_label->font();
-	font.setFamily(appfont.family());
-	font.setPixelSize(12);	// 9pts
-	nrofswitches_label->setFont(font);
 }
 
